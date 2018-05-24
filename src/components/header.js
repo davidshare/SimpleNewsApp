66f8ff9cf899f0db5e1bdbf19738621062791d4f
David Essien <davidessienshare@gmail.com>
2018-05-24 04:47:44 +0100

    attach the inputChange method to input field

--- src/components/header.js
+++ src/components/header.js
@@ -12,7 +12,7 @@ class Header extends Component{
 				<header>
 					<div className="logo"
 					onClick={()=>console.log('clicked')}>Logo</div>
-					<input/>
+					<input onChange{this.inputChange}/>
 				</header>
 			)
 	}
