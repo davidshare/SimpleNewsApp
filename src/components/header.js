3fc1ad7c179bd2cdaffcd01ca824b2a80b0e9790
David Essien <davidessienshare@gmail.com>
2018-05-24 04:51:13 +0100

    add constructor to control state

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,14 @@ import React, {Component} from 'react';
 //header component
 class Header extends Component{
 
+	constructor(props){
+		super(props);
+
+		this.state = {
+			keywords: 'Jon'
+		}
+	}
+
 	inputChange(event){
 		console.log(event.target.value);
 	}
