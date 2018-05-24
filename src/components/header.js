de08a1ccb1d5abfb9c07b2e77daa21922dae8bd1
David Essien <davidessienshare@gmail.com>
2018-05-24 03:47:50 +0100

    change header component from functional to class based component

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Header = ()=>{
-	return <div>
-		Header
-	</div>
+//header component
+class Header extends Component{
+	render(){
+		return(
+				<header>
+					<div>Logo</div>
+					<input/>
+				</header>
+			)
+	}
 }
-
 export default Header;
